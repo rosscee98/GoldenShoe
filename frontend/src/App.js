91c6b11b0d37a115133a5b87ac2bd90f1cae0c9f
Ross Clark <2251999c@student.gla.ac.uk>
2020-09-28 20:32:37 +0100

    Updates to home screen + NavBar styling * Home screen now hides carousel on smaller devices * Home screen now has shuffling tagline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,53 +12,71 @@ import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
     <Router>
-      <Navbar bg="light" expand="md">
-        <Navbar.Brand href="#home">
-          <img src="/images/logo192.png" width="35" height="35" alt="Golden Shoe" />
-          <p>Golden Shoe</p>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <NavDropdown title="Mens" id="dropdown-mens">
-              <LinkContainer to="/mens?kind=trainers">
-                <NavDropdown.Item>Trainers</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/mens?kind=shoes">
-                <NavDropdown.Item>Shoes</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/mens?kind=boots">
-                <NavDropdown.Item>Boots</NavDropdown.Item>
-              </LinkContainer>
-            </NavDropdown>
-            <NavDropdown title="Womens" id="dropdown-womens">
-              <LinkContainer to="/womens?kind=trainers">
-                <NavDropdown.Item>Trainers</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/womens?kind=shoes">
-                <NavDropdown.Item>Shoes</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/womens?kind=boots">
-                <NavDropdown.Item>Boots</NavDropdown.Item>
-              </LinkContainer>
-            </NavDropdown>
-            <NavDropdown title="Kids" id="dropdown-kids">
-              <LinkContainer to="/kids?kind=trainers">
-                <NavDropdown.Item>Trainers</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/kids?kind=shoes">
-                <NavDropdown.Item>Shoes</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/kids?kind=boots">
-                <NavDropdown.Item>Boots</NavDropdown.Item>
-              </LinkContainer>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Route path="/" exact component={HomeScreen} />
-      <Route path="/product/:id" component={ProductScreen} />
+      <div className="grid-container">
+        <header className="header">
+          <Navbar bg="light" expand="md" className="round-edge mx-4 my-3">
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <img
+                  src="/images/logo192.png"
+                  width="35"
+                  height="35"
+                  alt="Golden Shoe"
+                />{' '}
+                Golden Shoe
+              </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <NavDropdown title="Mens" id="dropdown-mens">
+                  <LinkContainer to="/mens?kind=trainers">
+                    <NavDropdown.Item>Trainers</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/mens?kind=shoes">
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/mens?kind=boots">
+                    <NavDropdown.Item>Boots</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+                <NavDropdown title="Womens" id="dropdown-womens">
+                  <LinkContainer to="/womens?kind=trainers">
+                    <NavDropdown.Item>Trainers</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/womens?kind=shoes">
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/womens?kind=boots">
+                    <NavDropdown.Item>Boots</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+                <NavDropdown title="Kids" id="dropdown-kids">
+                  <LinkContainer to="/kids?kind=trainers">
+                    <NavDropdown.Item>Trainers</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/kids?kind=shoes">
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/kids?kind=boots">
+                    <NavDropdown.Item>Boots</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+        <div className="main">
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </div>
+        <footer className="footer" fixed="bottom">
+          All rights reserved.
+        </footer>
+      </div>
     </Router>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,60 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
+import TextLoop from 'react-text-loop';
 
 function HomeScreen(props) {
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
-
-    return <Carousel activeIndex={index} onSelect={handleSelect} interval={5000}>
-        <Carousel.Item>
-            <img 
-                className="d-block w-100"
-                src="/images/grey.jpg"
-                alt="First slide"
-            />
-            <Carousel.Caption>
-                <h3>50% off men's boots</h3>
-                <p>Valid while stocks last</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img 
-                className="d-block w-100"
-                src="/images/grey.jpg"
-                alt="Second slide"
-            />
-            <Carousel.Caption>
-                <h3>Women's shoes</h3>
-                <p>Get em here pals</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
+    return <Container className="px-4 pt-0 pb-3" fluid>
+        <div className="carousel-wrapper d-none d-md-block">
+            <Carousel className="round-edge" interval={5000} fade="true">
+                <Carousel.Item>
+                    <img 
+                        className="d-block w-100"
+                        src="/images/promo/promo1.jpg"
+                        alt="First slide"
+                        // width={1392}
+                        // height={640}
+                    />
+                    <Carousel.Caption className="d-block bg-dark">
+                        <h3>50% off men's boots</h3>
+                        <p>Valid while stocks last</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img 
+                        className="d-block w-100"
+                        src="/images/grey.jpg"
+                        alt="Second slide"
+                        // width={1392}
+                        // height={640}
+                    />
+                    <Carousel.Caption className="bg-dark">
+                        <h3>Women's shoes</h3>
+                        <p>Get em here pals</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img 
+                        className="d-block w-100"
+                        src="/images/grey.jpg"
+                        alt="Third slide"
+                        // width={1392}
+                        // height={640}
+                    />
+                    <Carousel.Caption className="bg-dark">
+                        <h3>wee</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+        <hr className="d-none d-md-block"/>
+        <h3 id="tagline">
+            Find your next <TextLoop interval="5000" mask="true">
+                <span>dance the night away</span>
+                <span>5-a-side</span>
+                <span>working late</span>
+            </TextLoop> shoes
+        </h3>
+    </Container>
 }
 
 export default HomeScreen;
