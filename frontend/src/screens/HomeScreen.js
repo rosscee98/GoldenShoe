f100d9813d45a027935a83056ff6f36f95e1069b
Ross Clark <2251999c@student.gla.ac.uk>
2020-09-28 23:09:19 +0100

    Further changes to App.js and HomeScreen.js * Footer now remains at bottom of body, or bottom of screen if body is empty * Carousel captions now link to pages * Extra promo images now used

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,59 +1,67 @@
 import React, { useState } from 'react';
 import { Carousel, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import TextLoop from 'react-text-loop';
 
 function HomeScreen(props) {
     return <Container className="px-4 pt-0 pb-3" fluid>
         <div className="carousel-wrapper d-none d-md-block">
-            <Carousel className="round-edge" interval={5000} fade="true">
+            <Carousel className="round-edge" interval={5000}>
                 <Carousel.Item>
                     <img 
                         className="d-block w-100"
-                        src="/images/promo/promo1.jpg"
+                        src="/images/promo/mensboots.jpg"
                         alt="First slide"
-                        // width={1392}
-                        // height={640}
                     />
-                    <Carousel.Caption className="d-block bg-dark">
-                        <h3>50% off men's boots</h3>
-                        <p>Valid while stocks last</p>
+                    <Carousel.Caption className="round-edge">
+                        <Link to="/">
+                            <h3>50% off men's boots</h3>
+                            <p>Valid during October</p>
+                        </Link>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img 
                         className="d-block w-100"
-                        src="/images/grey.jpg"
+                        src="/images/promo/womensheels.jpg"
                         alt="Second slide"
-                        // width={1392}
-                        // height={640}
                     />
-                    <Carousel.Caption className="bg-dark">
-                        <h3>Women's shoes</h3>
-                        <p>Get em here pals</p>
+                    <Carousel.Caption className="round-edge">
+                        <Link to="/">
+                            <h3>Women's heels</h3>
+                            <p>100's to choose from</p>
+                        </Link>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img 
                         className="d-block w-100"
-                        src="/images/grey.jpg"
+                        src="/images/promo/running.jpg"
                         alt="Third slide"
-                        // width={1392}
-                        // height={640}
                     />
-                    <Carousel.Caption className="bg-dark">
-                        <h3>wee</h3>
+                    <Carousel.Caption className="round-edge">
+                        <Link to="/">
+                            <h3>Women's running shoes</h3>
+                            <p>New range available</p>
+                        </Link>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
         <hr className="d-none d-md-block"/>
-        <h3 id="tagline">
-            Find your next <TextLoop interval="5000" mask="true">
-                <span>dance the night away</span>
-                <span>5-a-side</span>
-                <span>working late</span>
-            </TextLoop> shoes
-        </h3>
+        <div id="tag">
+            <h3 id="tagline">
+                Find your next <TextLoop interval="5000" mask="true">
+                    <span>dance the night away</span>
+                    <span>5-a-side</span>
+                    <span>working late</span>
+                    <span>6am run</span>
+                    <span>munro bagging</span>
+                    <span>beach day</span>
+                </TextLoop> shoes
+            </h3>
+            <p>Whatever lies ahead, dive in now and find the shoes that'll get you through.</p>
+        </div>
     </Container>
 }
 
