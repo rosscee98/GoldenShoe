ee0d1a6741a491c7558389b1a0fb5b0e53ef741f
Ross Clark <2251999c@student.gla.ac.uk>
2020-09-30 18:50:51 +0100

    Mens products page now fetches data from db

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,16 @@
+import { PRODUCT_LIST_FAILURE, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+
+function productListReducer(state = { loading: true, products: [] }, action) {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST:
+            return {loading: true, products: []};
+        case PRODUCT_LIST_SUCCESS:
+            return {loading: false, products: action.payload};
+        case PRODUCT_LIST_FAILURE:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+export { productListReducer };
--- frontend/src/screens/MensScreen.js
+++ frontend/src/screens/MensScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Breadcrumb, Card, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function MensScreen(props) {
-    return <Container id="mens-container" className="px-4 pt-0 pb-3" fluid>
+    // console.log(state);
+    const productList = useSelector(state => state.productList);
+    console.log('start');
+    console.log(productList);
+    console.log('end');
+    const { products, loading, error } = productList;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(listProducts());
+        return () => {}
+    }, [])
+
+    return loading ? <h3 className="px-4">Loading...</h3> :
+    error ? <h3 className="px-4">{error}</h3> :
+    <Container id="mens-container" className="px-4 pt-0 pb-3" fluid>
         {/* className="round-edge"  */}
         <Breadcrumb>
             <LinkContainer to="/">
@@ -14,135 +31,48 @@ function MensScreen(props) {
         </Breadcrumb>
         <div className="products-wrapper px-3 py-2 mt-0 round-edge-bottom">
             <ul className="products">
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
-                <li>
-                    <Card className="product">
-                        <Link to="/product/1">
-                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
-                        </Link>
-                        <Card.Body>
-                            <Link to="/product/1">
-                                <Card.Title>Nike Air Force 1 '07</Card.Title>
-                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
-                                {/* <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button> */}
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </li>
+                {
+                    products.map(product => {
+                        return <li key={product._id}>
+                            <Card className="product">
+                                <Link to={ "/product/" + product._id }>
+                                    <Card.Img variant="top" src={ product.image } />
+                                </Link>
+                                <Card.Body>
+                                    <Link to={ "/product/" + product._id }>
+                                        <Card.Title>{ product.name }</Card.Title>
+                                        <Card.Subtitle className="text-muted">£{ product.price }</Card.Subtitle>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                    })
+                }
             </ul>
+            {/* <ul className="products">
+                <li>
+                    <Card className="product">
+                        <Link to="/product/1">
+                            <Card.Img variant="top" src="/images/products/mens/product1/img1.jpg" />
+                        </Link>
+                        <Card.Body>
+                            <Link to="/product/1">
+                                <Card.Title>Nike Air Force 1 '07</Card.Title>
+                                <Card.Subtitle className="text-muted">£84.95</Card.Subtitle>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                </li>
+            </ul> */}
         </div>
     </Container>
 }
+
+// const dispatch = useDispatch();
+
+// useEffect(() => {
+//     dispatch(listProducts());
+//     return () => {}
+// }, [])
 
 export default MensScreen;
