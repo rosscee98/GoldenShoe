72c8667f09c32d4f31558949496b0f9e998bfb51
Ross Clark <2251999c@student.gla.ac.uk>
2020-10-05 14:14:50 +0100

    Progress made on filtering * Able to apply some filters based on product properties * Not working correctly when trying to apply multiple property filters * Also need to implement filtering based on price + size if wanted

--- frontend/src/screens/MensScreen.js
+++ frontend/src/screens/MensScreen.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Breadcrumb, Card, Container, Form, Spinner } from 'react-bootstrap';
+import { Breadcrumb, Card, Col, Container, Form, Row, Spinner } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { listProducts, listProductsInCategory } from '../actions/productActions';
+import { listFilteredProducts, listProducts, listProductsInCategory } from '../actions/productActions';
 import { AiOutlineHome } from 'react-icons/ai';
+import ProductList from '../components/ProductList';
 
 function MensScreen(props) {
     const productList = useSelector(state => state.productList);
@@ -13,50 +14,102 @@ function MensScreen(props) {
     const dispatch = useDispatch();
 
     useEffect(() => {
+        console.log(properties);
         dispatch(listProductsInCategory("Mens"));
         return () => {}
     }, [])
 
-    return loading ? <Spinner className="mx-4" animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-    </Spinner> :
-    error ? <h3 className="mx-4">{error}</h3> :
-    <Container className="content-container px-4 pt-0 pb-3" fluid>
+    useEffect(() => {
+        dispatch(listFilteredProducts(properties, "Mens"));
+        console.log(properties);
+        return () => {}
+    }, [properties])
+
+    const handlePropertyChange = (selected, value) => {
+        console.log(properties);
+        console.log(selected, value);
+        selected
+            ? setProperties(properties => [...properties, value])
+            : setProperties(properties => properties.filter(p => p !== value));
+    }
+
+    return <Container className="content-container px-4 pt-0 pb-3" fluid>
         <Breadcrumb>
             <LinkContainer to="/">
                 <Breadcrumb.Item>
                     <AiOutlineHome />
                 </Breadcrumb.Item>
-                {/* <Breadcrumb.Item>Home</Breadcrumb.Item> */}
             </LinkContainer>
             <Breadcrumb.Item active="true">Mens</Breadcrumb.Item>
         </Breadcrumb>
-        <div className="bg-dark-grey px-3 py-2 mt-0 round-edge-bottom">
-            {/* <Form>
-                <Form.Check label="Black" type="checkbox" id="black-checkbox" />
-                <Form.Check label="Blue" type="checkbox" id="blue-checkbox" />
-            </Form> */}
-            <p className="text-muted mb-0">{ products.length } products found</p>
-            <ul className="products">
+        {/* <div className="round-edge-bottom" style={{'display': 'flex', 'flex-wrap': 'wrap'}}> */}
+        {/* <div className="round-edge-bottom"> */}
+        <Row className="round-edge-bottom m-0">
+            {/* <div className="bg-light-grey" style={{'border-top': '.1rem solid lightgrey', 'border-right': '.1rem solid lightgrey', 'flex': '1 1 20rem'}}> */}
+            {/* <div className="bg-light-grey"> */}
+            <Col className="bg-light-grey" sm={3} style={{'border-bottom-left-radius': '1rem'}}>
+                <h3>Filters</h3>
+                <h4 className="text-muted">Colour</h4>
+                <Form>
+                    {
+                        ["Black", "Blue", "Red", "Yellow", "Green"].map(colour => (
+                            <Form.Check
+                                label={ colour }
+                                type="checkbox"
+                                value={ colour }
+                                onChange={ e => handlePropertyChange(e.target.checked, colour) }
+                                defaultChecked={ e => properties.indexOf(colour !== -1) }
+                            />
+                        ))
+                    }
+                </Form>
+                <hr />
+                <h4 className="text-muted">Size</h4>
+                <Form>
+                    {
+                        [...Array(12).keys()].map(i => (
+                            <Form.Check
+                                label={ i+1 }
+                                type="checkbox"
+                                value={ i+1 }
+                                onChange={ e => handlePropertyChange(e.target.checked, i+1) }
+                                defaultChecked={ e => properties.indexOf((i+1) !== -1) }
+                            />
+                        ))
+                    }
+                </Form>
+                <hr />
+                <h4 className="text-muted">Price</h4>
+                <Form>
+                    {
+                        ["Under £50", "£50-75", "£75-£100", "£100-125", "£125-150", "Over £150"].map(price => (
+                            <Form.Check
+                                label={ price }
+                                type="checkbox"
+                                value={ price }
+                                onChange={ e => handlePropertyChange(e.target.checked, price) }
+                                defaultChecked={ e => properties.indexOf(price !== -1) }
+                            />
+                        ))
+                    }
+                </Form>
+            </Col>
+            {/* </div> */}
+            {/* <div className="bg-dark-grey px-3 py-2 mt-0"> */}
+            <Col className="bg-dark-grey px-3 py-2 mt-0" sm={9} style={{'border-bottom-right-radius': '1rem'}}>
                 {
-                    products.map(product => {
-                        return <li key={product._id}>
-                            <Card className="product">
-                                <Link to={ "/product/" + product._id }>
-                                    <Card.Img variant="top" src={ product.image } />
-                                </Link>
-                                <Card.Body>
-                                    <Link to={ "/product/" + product._id }>
-                                        <Card.Title>{ product.name }</Card.Title>
-                                        <Card.Subtitle className="text-muted">£{ product.price }</Card.Subtitle>
-                                    </Link>
-                                </Card.Body>
-                            </Card>
-                        </li>
-                    })
+                    loading
+                        ? <Spinner className="mx-4" animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                        : error
+                            ? <h3 className="mx-4">{error}</h3> 
+                            : <ProductList products={ products } />
                 }
-            </ul>
-        </div>
+            </Col>
+            {/* </div> */}
+        </Row>
+        {/* </div> */}
     </Container>
 }
 
