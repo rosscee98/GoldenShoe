8edf746a011446f2ecc1ada36540be63c0990a1a
Ross Clark <2251999c@student.gla.ac.uk>
2020-10-06 12:54:50 +0100

    Rejigged BasketScreen design

--- frontend/src/screens/BasketScreen.js
+++ frontend/src/screens/BasketScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { addToBasket, removeFromBasket } from '../actions/basketActions';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CounterInput from "react-counter-input";
 
@@ -26,55 +26,53 @@ function BasketScreen(props) {
         };
     }, [dispatch, productId]);
 
-    //228,231,235
     return <Container className="content-container px-4 pt-0 pb-3" fluid>
-        <div className="bg-light-grey px-3 py-2 mt-0 round-edge">
-            <div className="basket">
-                <div className="basket-list">
-                    <ul className="basket-list-container">
-                        <li>
-                            <h3>Shopping Basket</h3>
-                            <div>Price</div>
-                        </li>
-                        {
-                            basketItems.length === 0
-                                ? <p>Basket is empty.</p>
-                                : basketItems.map(item => 
-                                    <li>
-                                        <div className="cart-image">
-                                            <img src={ item.image } alt="product" />
-                                        </div>
-                                        <div className="product-name">
-                                            <div>
-                                                <Link to={ "/product/" + item.product }>{ item.name }</Link>
-                                            </div>
-                                            <div>
+        <div className="basket bg-light-grey px-3 py-2 mt-0 round-edge">
+            <div className="basket-list">
+                <ul className="basket-list-container">
+                    <li>
+                        <h3>Basket</h3>
+                    </li>
+                    {
+                        basketItems.length === 0
+                            ? <p>Basket is empty.</p>
+                            : basketItems.map(item => 
+                                <li>
+                                    <Row className="product-row px-3 w-100">
+                                        <Col xs={12} sm={6} md={3}>
+                                            <Image src={ item.image } alt={ item.name } />
+                                        </Col>
+                                        <Col xs={12} sm={6} md={3} className="bottom">
+                                                <Link to={ "/product/" + item.product }>
+                                                    <h3>{ item.name }</h3>
+                                                </Link>
                                                 <p className="text-muted">Size { item.size }</p>
-                                            </div>
-                                            <div>
                                                 Qty:
-                                                <CounterInput
+                                                <CounterInput 
                                                     count={ item.qty }
-                                                    min={ 1 }
+                                                    min={1}
                                                     max={ Math.min(item.countInStock, 30) }
                                                     onCountChange={ count => dispatch(addToBasket(item.product, item.size, item.countInStock, count)) }
                                                 />
-                                                <button type="button" className="button" onClick={ () => handleRemoveFromBasket(item.product, item.size) }>Delete</button>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            { item.price }
-                                        </div>
-                                    </li>    
-                                )
-                        }
-                    </ul>
-                </div>
-                <div className="basket-action">
-                    <h3>Subtotal ({ basketItems.reduce((acc, cur) => acc + parseInt(cur.qty), 0) } items):
-                        £{ basketItems.reduce((acc, cur) => acc + cur.price * cur.qty, 0).toFixed(2) }</h3>
-                    <button onClick={ () => handleCheckout() } className="button primary w-100" disabled={ basketItems.length === 0 }>Proceed to checkout</button>
-                </div>
+                                        </Col>
+                                        <Col xs={12} sm={6} md={3} className="bottom">
+                                            <Button variant="danger" onClick={ () => handleRemoveFromBasket(item.product, item.size) }>
+                                                Delete
+                                            </Button>
+                                        </Col>
+                                        <Col xs={12} sm={6} md={3} className="bottom">
+                                            <h3 className="text-muted">£{ (item.price * item.qty).toFixed(2) }</h3>
+                                        </Col>
+                                    </Row>
+                                </li>    
+                            )
+                    }
+                </ul>
+            </div>
+            <div className="basket-action">
+                <h3>Subtotal ({ basketItems.reduce((acc, cur) => acc + parseInt(cur.qty), 0) } items):
+                    £{ basketItems.reduce((acc, cur) => acc + cur.price * cur.qty, 0).toFixed(2) }</h3>
+                <Button className="w-100 mt-2" onClick={ () => handleCheckout() } disabled={ basketItems.length === 0 }>Proceed to checkout</Button>
             </div>
         </div>
     </Container>
